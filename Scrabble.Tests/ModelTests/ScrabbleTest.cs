92d107ef873a34acb58df9469aa62d634c6e440d
Jimmy McNamara <jmcnamara44@gmail.com>
2018-04-26 14:07:43 -0700

    pass the second test, users will pass in a word and it will be returned as an array.

--- Scrabble.Tests/ModelTests/ScrabbleTest.cs
+++ Scrabble.Tests/ModelTests/ScrabbleTest.cs
@@ -13,11 +13,26 @@ namespace Scrabble.Tests
 
     }
     [TestMethod]
-    public void ReturnWord_ReturnsWord_Word()
+    public void GetWord_ReturnsWord_Word()
     {
       string testWord1 = "abc";
       Letter newWord = new Letter(testWord1);
       Assert.AreEqual(testWord1, newWord.GetWord());
+    }
+    [TestMethod]
+    public void WordToList_ReturnsWordInAList_WordInAList()
+    {
+      Letter testWord1 = new Letter("a");
+      Letter testWord2 = new Letter("b");
+      Letter testWord3 = new Letter("c");
+      List<Letter> testList1 = new List<Letter> {testWord1, testWord2, testWord3};
+      Letter newWord = new Letter("abc");
+      Letter.WordToList(newWord);
+      List<Letter> testList2 = Letter.GetAll();
+      Assert.AreEqual(testList1[0].GetWord(), testList2[0].GetWord());
+      Assert.AreEqual(testList1[1].GetWord(), testList2[1].GetWord());
+      Assert.AreEqual(testList1[2].GetWord(), testList2[2].GetWord());
+      Assert.AreEqual(testList1.Count, testList2.Count);
     }
   }
 }
--- Scrabble/Models/Scrabble.cs
+++ Scrabble/Models/Scrabble.cs
@@ -7,15 +7,39 @@ namespace Scrabble.Models
     public class Letter
     {
         private string _word;
+
         private static Dictionary<string, int> scores = new Dictionary<string, int> { {"A", 1}, {"B", 3}, {"C", 3}, {"D", 2}, {"E", 1}, {"F", 4}, {"G", 2}, {"H", 4}, {"I", 1}, {"J", 8}, {"K", 5}, {"L", 1}, {"M", 3}, {"N", 1}, {"O", 1}, {"P", 3}, {"Q", 10}, {"R", 1}, {"S", 1}, {"T", 1}, {"U", 1}, {"V", 4}, {"W", 4}, {"X", 8}, {"Y", 4}, {"Z", 10} };
+
+        private static List<Letter> _letters = new List<Letter> {};
 
         public Letter(string word)
         {
           _word = word;
         }
+
         public string GetWord()
         {
           return _word;
+        }
+
+        public static void WordToList(Letter word)
+        {
+          char[] wordArray = word.GetWord().ToCharArray();
+          for (int x = 0; x<wordArray.Length; x++)
+          {
+            Letter testStringx = new Letter(String.Join("", wordArray[x]));
+            testStringx.Save();
+          }
+        }
+
+        public void Save()
+        {
+          _letters.Add(this);
+        }
+
+        public static List<Letter> GetAll()
+        {
+          return _letters;
         }
     }
 }
